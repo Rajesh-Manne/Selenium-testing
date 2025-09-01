416b54c88577b31dd78d96e378b5622944d6106f
Rajesh Manne <mannerajesh99@gmail.com>
2025-09-01 19:04:20 +0530

    second commit

--- test file.js
+++ test file.js
@@ -4,3 +4,9 @@ describe('Google Search', () => {
     cy.title().should('include', 'Google')
   })
 })
+describe('Google Search', () => {
+  it('should load Google homepage and check the title', () => {
+    cy.visit('https://www.yahoo.com')
+    cy.title().should('include', 'Google')
+  })
+})
